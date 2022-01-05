3a78d892e92791a7acb53b5e941ed1a85695441f
kangactor123 <kangactor123@naver.com>
2022-01-05 10:32:39 +0900

    login_interceptor

--- Final_eatDa/src/main/java/com/project/eatda/common/interceptor/LoginInterceptor.java
+++ Final_eatDa/src/main/java/com/project/eatda/common/interceptor/LoginInterceptor.java
@@ -6,14 +6,10 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class LoginInterceptor implements HandlerInterceptor {
-	
-	private Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
 	public String[] permitCommand = {
 			"/index.do", "/idCheck.do","/regist_form.do","/emailCheck.do","/memberRegist.do","/loginCheck.do",
 			"/foodbti.do","/getMarketData.do","/getRecentRecipe.do","/getMbtiProduct.do","/getPopularBlog.do","/getLovingRecipe.do",
@@ -22,12 +18,8 @@ public class LoginInterceptor implements HandlerInterceptor {
 			"/recipeList.do","/recipeListTest.do","/recipeCategory.do","/recipeDetail.do"
 	};
 	
-	//함부러 요청이 넘어가지 않도록
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		//boolean 으로 컨트롤러로 요청을 넣을지 안넣을지 false : 반환 true : 정상처리
-		logger.info("[interceptor.preHandler] request :"+request.getRequestURI());
-		
 		if (request.getSession().getAttribute("member") != null) {
 			return true;
 		} 
@@ -40,7 +32,6 @@ public class LoginInterceptor implements HandlerInterceptor {
 		
 		if (request.getSession().getAttribute("member") == null) {
 			jsResponse("index.do","Please Log in!", response);
-			//response.sendRedirect("index.do");
 		}
 		return false;
 		
@@ -48,14 +39,10 @@ public class LoginInterceptor implements HandlerInterceptor {
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		logger.info("[interceptor]: postHandle");
-		
 	}
 
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		logger.info("[interceptor]: afterCompletion");
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 	}
 	
 	public void jsResponse(String url, String message, HttpServletResponse response) throws IOException {
