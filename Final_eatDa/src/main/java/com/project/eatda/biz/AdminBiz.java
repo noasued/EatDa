4c44d5f5dfe9b4a137634b799c249fdabd3b74c0
YJ <yedoong919@gmail.com>
2022-01-12 23:24:31 +0900

    test Commit

--- Final_eatDa/src/main/java/com/project/eatda/biz/AdminBiz.java
+++ Final_eatDa/src/main/java/com/project/eatda/biz/AdminBiz.java
@@ -49,7 +49,6 @@ public interface AdminBiz {
 	public List<UserDto> adminUserList();
 	public int adminUserUpdate(UserDto dto);
 	public int adminUserDelete(String user_id);
-
 	//이메일
 	public void sendMail(EmailDto dto);
 	
